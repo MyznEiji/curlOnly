f3b960f78d8b3519a8ffa8e2400b077e8f1250bb
MyznEiji <em711th@icloud.com>
2018-06-22 16:26:23 +0900

    fix

--- fetch_test.sh
+++ fetch_test.sh
@@ -6,14 +6,14 @@ git_dir=~/Desktop/git-flow-tutorial/
 
 cd $desc
 git clone https://github.com/MyznEiji/git-flow-tutorial.git
-# cd $git_dir
-# git checkout -b $1
-# touch $2.txt
-# echo fetch_test > $2.txt
-# git add .
-# git commit -m fetch_test
-# git push origin $1
-# open https://github.com/MyznEiji/git-flow-tutorial
+cd $git_dir
+git checkout -b $1
+touch $2.txt
+echo fetch_test > $2.txt
+git add .
+git commit -m fetch_test
+git push origin $1
+open https://github.com/MyznEiji/git-flow-tutorial
 
 echo " remove directory [Enter] "
 read
