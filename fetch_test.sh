e6b62d089dc2e09d6a8cd4e6e9e7d42f0b34598f
MyznEiji <em711th@icloud.com>
2018-06-22 16:20:22 +0900

    hoge

---
+++ fetch_test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# arg1=BRANCH_NAME  arg2=FILE_NAME
+
+desc=~/Desktop
+git_dir=~/Desktop/git-flow-tutorial/
+
+cd $desc
+git clone https://github.com/MyznEiji/git-flow-tutorial.git
+# cd $git_dir
+# git checkout -b $1
+# touch $2.txt
+# echo fetch_test > $2.txt
+# git add .
+# git commit -m fetch_test
+# git push origin $1
+# open https://github.com/MyznEiji/git-flow-tutorial
+
+echo " remove directory [Enter] "
+read
+
+rm -rf $git_dir
+rm -f fetch_test.sh
